ac161b2e472f07168ff6f58d4bc4c24ef91e88e6
nikbarsa <99321419+nikbarsa@users.noreply.github.com>
2023-01-08 22:31:49 +0300

    add css

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
-$wishes = ['здоровья' ,'удачи', 'счастья',];
-$epit = ['наилучшего','постоянного', 'неверотяного'];
-$name = readline('введите ваше имя');
-$rand = array_rand($wishes);
-$rands = array_rand($epit);
-echo "дорогой $name поздравляю тебя с днем рождения желаю $epit[$rands] $wishes[$rand]";
 
-<?php
-$arr1 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-$arr2 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-$multi = [];
-foreach ($arr1 as $key => $value) {
-    $multi[] = $value * $arr2[$key];
-}
 
-print_r($multi);
+$number = [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2];
+$rand = array_rand($number);
+if (($number[$rand] % 2) == 0) {
+    echo "четное";
+}elseif ( ($number[$rand] % 2) !== 0) {
+   echo "не четное";
+};
+
+
+$numbers = [4, 5, 1, 4];
+$max = max($numbers);
+$min = min($numbers);
+$numbers = array_filter($numbers);
+$average = array_sum($numbers)/count($numbers);
+echo $average;
+$numm = [$max,$min,$average];
+var_dump($numm);
+
